72d01fb5520c84f5cdac40be3bb320066823200d
Gabriel Lisboa <gbr.lisboa@gmail.com>
2024-10-03 10:45:31 -0300

    search linkedList method

--- linkedList.c
+++ linkedList.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+#define true 1
+#define false 0
 
 struct Node {
     int data;
@@ -7,13 +11,31 @@ struct Node {
 };
 
 void transverseLinkedList(struct Node *head);
+bool searchLinkedList(struct Node *head, int target);
 
 int main(void) {
    struct Node* head =  malloc(sizeof(struct Node));
+    struct Node* second = malloc(sizeof(struct Node));
+    struct Node* third = malloc(sizeof(struct Node));
+
+    head->data = 10;
+    head->next = second;
+    second->data = 20;
+    second->next = third;
+    third->data = 30;
+    third->next = nullptr;
+
+    transverseLinkedList(head);
+    printf("%d",searchLinkedList(head, 30));
+
     free(head);
+    free(second);
+    free(third);
+
+    return 0;
 }
 
-void traverseLinkedList(struct Node* head){
+void transverseLinkedList(struct Node* head){
 
     struct Node* current = head;
 
@@ -23,4 +45,14 @@ void traverseLinkedList(struct Node* head){
     }
 
     printf("\n");
+}
+
+bool searchLinkedList(struct Node* head, int target) {
+    while(head != NULL) {
+        if(head->data == target) {
+            return true;
+        }
+        head = head->next;
+    }
+        return false;
 }
